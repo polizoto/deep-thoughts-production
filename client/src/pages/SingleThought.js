cd5456f5895fc49a2e2ee58d69e69b508224f641
polizoto <polizoto@hotmail.com>
2022-04-06 17:57:48 -0400

    added Reaction Form

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
+import Auth from '../utils/auth';
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -29,6 +31,7 @@ if (loading) {
     <div className="card-body">
       <p>{thought.thoughtText}</p>
     </div>
+    {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
   </div>
 
   {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
